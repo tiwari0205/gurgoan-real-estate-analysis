2848923ba31774ba315c0b9cd0ef068e7c1f2d98
Daksh Tiwari <tiwaridaksh555@gmail.com>
2026-05-26 11:37:46 +0530

    code updated

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ else:
     print("RERA-approved properties do not command a price premium.")
 
 # Question 6: How does area impact price?
-# sns.scatterplot(data=df, x='area', y='price')
-# plt.show()
+sns.scatterplot(data=df, x='area', y='price')
+plt.show()
 
 # Question 7: Which BHK configuration is most expensive based on per sqft rate?
 most_expensive_bhk = df.groupby('bhk_count')['rate_per_sqft'].mean().idxmax()
